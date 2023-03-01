f2a710b2f3c9e5700604fa0458a0b7fa1a158a5d
Thrive <guillaumemengelatte@hotmail.fr>
2023-03-01 14:15:51 +0100

    reset repo

--- config/nodemail.config.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-  service: 'localhost',
-  port: 25,
-  secure: false,
-})
-
-module.exports = transporter;
