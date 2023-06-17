a8b624d3d319fd979aa7ab8dacc2eedcdb0392a6
Thrive <guillaumemengelatte@hotmail.fr>
2023-06-17 15:59:51 +0200

    posts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 8000;
@@ -8,6 +9,16 @@ async function bootstrap() {
   app.setGlobalPrefix('api', {
     exclude: ['/']
   });
+
+  const config = new DocumentBuilder()
+  .setTitle('StudyJobs')
+  .setDescription('API Documentation')
+  .setVersion('1.0')
+  .addTag('StudyJobs')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors(); 
   await app.listen(PORT);
 
