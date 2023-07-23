975dc3afcee51fb21473efe925c3647820aedbdb
GuillaumeM10 <guillaumemengelatte@hotmail.fr>
2023-07-23 17:28:16 +0200

    reset mail

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -8,18 +8,14 @@ export class MailService {
   constructor(private mailerService: MailerService) {}
 
   async sendUserConfirmation(user: UserEntity, token: string) {
-    const url = `example.com/auth/confirm?token=${token}`;
-
     try{
 
       await this.mailerService.sendMail({
         to: user.email,
-        // from: '"Support Team" <support@example.com>', // override default from
         subject: 'STYDYJob : Mail de confirmation',
-        template: './confirmation', // `.hbs` extension is appended automatically
-        context: { // ✏️ filling curly brackets with content
-          name: user.firstName,
-          url,
+        template: './confirmation',
+        context: {
+          name: user.firstName
         },
       });  
 
@@ -38,18 +34,17 @@ export class MailService {
       
       await this.mailerService.sendMail({
         to: createTokenResetPasswordDto.email,
-        // from: '"Support Team" <
         subject: 'STYDYJob : Mail de changement de mot de passe',
-        template: './reset-password', // `.hbs` extension is appended automatically
-        context: { // ✏️ filling curly brackets with content
-          url,
+        template: './reset-password',
+        context: {
+          url
         },
       });
       
-      return { message: `Mail envoyé à ${createTokenResetPasswordDto.email}`};
     
     }catch(err){
       console.log(err)
+      
       throw new ConflictException(`Le mail n'a pas pu être envoyé à ${createTokenResetPasswordDto.email}`);
     }
   }
